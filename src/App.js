f712a5914a6f707cd08153dea9ccc77a4c629238
Samer Bouachir <samer.imed.bou@gmail.com>
2021-06-16 11:43:34 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
+    const directory = this.state.campsites.map(campsite => {
+      return (
+        <div key={campsite.id} className="col">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">NuCamp</NavbarBrand>
+            </div>
+            );
+          </Navbar>
+          <Directory />
+        </div>
+      );
+    });
   }
+
 }
 
 export default App;
